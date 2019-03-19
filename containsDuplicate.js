4d0be4e8219a216cc762eedf625e635f0e8b630d
zj19941113 <41109686+zj19941113@users.noreply.github.com>
2019-03-19 11:31:08 +0800

    217	存在重复元素 47.0%	简单

---
+++ containsDuplicate.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort();
+    for(var i=0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+};
