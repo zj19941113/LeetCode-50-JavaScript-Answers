ebb21ea6c4c7d1b3262fe82dca5d08aa71ff337d
zj19941113 <41109686+zj19941113@users.noreply.github.com>
2019-03-19 11:21:28 +0800

    141	环形链表 35.5%	简单

---
+++ hasCycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+ 
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    var slow = head, fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+};
